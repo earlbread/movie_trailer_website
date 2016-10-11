463838907a7bbf82b417e0ef1b5514f4ca537b92
Seunghun Lee <waydi1@gmail.com>
2016-10-11 15:05:13 -0700

    Add docstring for Movie class

--- media.py
+++ media.py
@@ -1,4 +1,12 @@
 class Movie:
+    """This is Movie class for static site generation.
+
+    Attributes:
+        title (str) : Movie's title.
+        poster_image_url (str) : Box art image url.
+        trailer_youtube_url (str) : Movie trailer video url.
+
+    """
     def __init__(self, title, poster_image_url, trailer_youtube_url):
         self.title = title
         self.poster_image_url = poster_image_url
